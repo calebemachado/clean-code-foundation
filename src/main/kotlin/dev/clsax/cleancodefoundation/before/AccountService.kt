93af46502d850d29f6e181ded6ca3fddd8e15164
Calebe Machado <calebe.machado@ifood.com.br>
2023-09-29 14:58:28 -0300

    Refactor to abort early

--- src/main/kotlin/dev/clsax/cleancodefoundation/before/AccountService.kt
+++ src/main/kotlin/dev/clsax/cleancodefoundation/before/AccountService.kt
@@ -9,7 +9,6 @@ import io.vertx.sqlclient.Tuple
 import java.time.LocalDateTime
 import java.util.*
 
-
 class AccountService(private val vertx: Vertx) {
 
   fun sendEmail(email: String, subject: String, message: String) {
@@ -18,12 +17,10 @@ class AccountService(private val vertx: Vertx) {
 
   fun validateCpf(str: String): Boolean {
     if (str.length >= 11 && str.length <= 14) {
-
       var str = str
         .replace(".", "")
         .replace("-", "")
         .replace(" ", "")
-
       if (!str.all { it == str[0] }) {
         try {
           var d1: Int
@@ -38,28 +35,22 @@ class AccountService(private val vertx: Vertx) {
           dg1 = 0
           dg2 = 0
           rest = 0
-
           for (nCount in 1 until str.length - 1) {
             val digito = str[nCount - 1].toString().toInt()
             d1 += (11 - nCount) * digito
             d2 += (12 - nCount) * digito
           }
-
           rest = (d1 % 11)
-
           dg1 = if (rest < 2) 0 else (11 - rest);
-
           d2 += 2 * dg1
           rest = (d2 % 11)
           if (rest < 2) dg2 = 0
           else dg2 = 11 - rest
-
           var nDigVerific = str.substring(str.length - 2, str.length)
           nDigResult = "$dg1$dg2"
           return nDigVerific == nDigResult
         } catch (e: Exception) {
           println("Erro ! $e")
-
           return false
         }
       } else return false
@@ -76,63 +67,46 @@ class AccountService(private val vertx: Vertx) {
     val poolOptions = PoolOptions()
       .setMaxSize(5)
     val client = PgPool.client(vertx, connectOptions, poolOptions)
-
     val accountId = UUID.randomUUID()
     try {
       val verificationCode = UUID.randomUUID()
       val date = LocalDateTime.now()
-
       val rows = client.preparedQuery("select * from cccat13.account where email = $1")
         .execute(Tuple.of(input.email))
         .await()
-
       val account = if (rows.iterator().hasNext()) {
-          val next = rows.iterator().next()
+        val next = rows.iterator().next()
         Account(
           next.getUUID("account_id"),
           next.getString("name")
-          )
+        )
       } else {
         null
       }
-
-      if (account == null) {
-        if (input.name.matches(Regex("[a-zA-Z]+ [a-zA-Z]+"))) {
-          if (input.email.matches(Regex("^(.+)@(.+)\$"))) {
-
-            if (validateCpf(input.cpf)) {
-              if (input.isDriver) {
-                if (input.carPlate.matches(Regex("[A-Z]{3}[0-9]{4}"))) {
-                  client.preparedQuery("insert into cccat13.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver, date, is_verified, verification_code) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)")
-                    .execute(Tuple.of(accountId, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver, date, false, verificationCode))
-                    .await()
-                  sendEmail(input.email, "Verification", "Please verify your code at first login $verificationCode")
-                } else {
-                  throw Exception("Invalid plate")
-                }
-              } else {
-                client.preparedQuery("insert into cccat13.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver, date, is_verified, verification_code) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)")
-                  .execute(Tuple.of(accountId, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver, date, false, verificationCode))
-                  .await()
-
-                sendEmail(input.email, "Verification", "Please verify your code at first login $verificationCode")
-
-                return SignUpResponse(accountId)
-              }
-            } else {
-              throw Exception("Invalid cpf")
-            }
-          } else {
-            throw Exception("Invalid email")
-          }
-        } else {
-          throw Exception("Invalid name")
-        }
-      } else {
-        throw Exception("Account already exists")
-      }
+      if (account != null) throw Exception("Account already exists")
+      if (!input.name.matches(Regex("[a-zA-Z]+ [a-zA-Z]+"))) throw Exception("Invalid name")
+      if (!input.email.matches(Regex("^(.+)@(.+)\$")))  throw Exception("Invalid email")
+      if (!validateCpf(input.cpf)) throw Exception("Invalid cpf")
+      if (input.isDriver && !input.carPlate.matches(Regex("[A-Z]{3}[0-9]{4}"))) throw Exception("Invalid plate")
+      client.preparedQuery("insert into cccat13.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver, date, is_verified, verification_code) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)")
+        .execute(
+          Tuple.of(
+            accountId,
+            input.name,
+            input.email,
+            input.cpf,
+            input.carPlate,
+            !!input.isPassenger,
+            !!input.isDriver,
+            date,
+            false,
+            verificationCode
+          )
+        )
+        .await()
+      sendEmail(input.email, "Verification", "Please verify your code at first login $verificationCode")
     } finally {
-        client.close()
+      client.close()
     }
     return SignUpResponse(accountId)
   }
@@ -147,11 +121,9 @@ class AccountService(private val vertx: Vertx) {
     val poolOptions = PoolOptions()
       .setMaxSize(5)
     val client = PgPool.client(vertx, connectOptions, poolOptions)
-
     val rows = client.preparedQuery("select account_id, name from cccat13.account where account_id = $1")
       .execute(Tuple.of(accountId))
       .await()
-
     return if (rows.iterator().hasNext()) {
       val next = rows.iterator().next()
       Account(
